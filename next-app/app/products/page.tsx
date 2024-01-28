792fd33552d1b094da413f4e6a565906c31946b5
Meelenych <meelenych@gmail.com>
2024-01-28 11:50:24 -0700

    product card and list styles

--- next-app/app/products/page.tsx
+++ next-app/app/products/page.tsx
@@ -12,7 +12,7 @@ const dummyProducts: Product[] = [
 		id: '1',
 		name: 'cabbage',
 		price: 20,
-		description: 'Fresh cabbage every day',
+		description: 'Fresh cabbage every day.',
 		image:
 			'https://cdn.pixabay.com/photo/2018/10/03/22/08/kohl-3722517_960_720.jpg',
 	},
@@ -20,15 +20,15 @@ const dummyProducts: Product[] = [
 		id: '2',
 		name: 'parsley',
 		price: 30,
-		description: 'Green parsley, fresh and crispy',
+		description: 'Green parsley, fresh and crispy.',
 		image:
-			'https://media.istockphoto.com/id/155157821/photo/parsley-bouquet.jpg?s=2048x2048&w=is&k=20&c=Utve-Yy1rUrz8xvitpWnIPfs_9d4-pMup-YRgBZJ6Y0=',
+			'https://cdn.pixabay.com/photo/2016/03/29/01/06/cilantro-1287301_960_720.jpg',
 	},
 	{
 		id: '3',
 		name: 'cucumber',
 		price: 14,
-		description: 'Nice, refreshing and crunchy',
+		description: 'Nice, refreshing and crunchy.',
 		image:
 			'https://cdn.pixabay.com/photo/2015/07/17/13/44/cucumbers-849269_1280.jpg',
 	},
@@ -36,7 +36,7 @@ const dummyProducts: Product[] = [
 		id: '4',
 		name: 'garlic',
 		price: 50,
-		description: 'Add picancy to your dish',
+		description: 'Add picancy to your dish.',
 		image:
 			'https://cdn.pixabay.com/photo/2018/05/21/21/23/garlic-3419544_960_720.jpg',
 	},
@@ -44,7 +44,7 @@ const dummyProducts: Product[] = [
 		id: '5',
 		name: 'corn',
 		price: 20,
-		description: 'Sweet and juicy ',
+		description: 'Sweet and juicy.',
 		image:
 			'https://cdn.pixabay.com/photo/2015/05/26/17/42/pop-corn-785074_1280.jpg',
 	},
@@ -65,7 +65,7 @@ const ProductsPage: React.FC<ProductsPageProps> = ({
 	}, []);
 
 	return (
-		<ul>
+		<ul className='grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-4 gap-4 p-3 sm:p-6 lg:p-10'>
 			{products?.map(product => (
 				<ProductCard
 					key={product.id}
--- next-app/app/products/productCard/productCard.tsx
+++ next-app/app/products/productCard/productCard.tsx
@@ -4,13 +4,23 @@ import { Product as ProductType } from '../../interfaces/interfaces';
 const ProductCard = ({ product }: { product: ProductType }) => {
 	return (
 		<li>
-			<p>Name: {product.name}</p>
-			<p>Price: {product.price}</p>
-			<p>Description: {product.description}</p>
-			<img
-				src={product.image}
-				alt=''
-			/>
+			<div className='card card-compact bg-base-100 shadow-xl'>
+				<figure>
+					<img
+						src={product.image}
+						alt={product.name}
+					/>
+				</figure>
+				<div className='card-body'>
+					<h2 className='card-title'>
+						{product.name.charAt(0).toUpperCase() + product.name.slice(1)}
+					</h2>
+					<p>{product.description}</p>
+					<div className='card-actions justify-end'>
+						<button className='btn btn-primary'>Buy Now</button>
+					</div>
+				</div>
+			</div>
 		</li>
 	);
 };
