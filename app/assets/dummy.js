08be94947bad2442675ae9b470d2ce58f8d514d6
Meelenych <meelenych@gmail.com>
2024-02-07 01:11:23 -0700

    interactive rating added

--- app/assets/dummy.js
+++ app/assets/dummy.js
@@ -7,6 +7,7 @@ export const dummyItems = [
 		description: 'Fresh cabbage every day.',
 		image:
 			'https://cdn.pixabay.com/photo/2018/10/03/22/08/kohl-3722517_960_720.jpg',
+		rating: 3,
 	},
 	{
 		id: '2',
@@ -16,6 +17,7 @@ export const dummyItems = [
 		description: 'Green parsley, fresh and crispy.',
 		image:
 			'https://cdn.pixabay.com/photo/2016/03/29/01/06/cilantro-1287301_960_720.jpg',
+		rating: 1,
 	},
 	{
 		id: '3',
@@ -25,6 +27,7 @@ export const dummyItems = [
 		description: 'Nice, refreshing and crunchy.',
 		image:
 			'https://cdn.pixabay.com/photo/2015/07/17/13/44/cucumbers-849269_1280.jpg',
+		rating: 2,
 	},
 	{
 		id: '4',
@@ -34,6 +37,7 @@ export const dummyItems = [
 		description: 'Add picancy to your dish.',
 		image:
 			'https://cdn.pixabay.com/photo/2018/05/21/21/23/garlic-3419544_960_720.jpg',
+		rating: 4,
 	},
 	{
 		id: '5',
@@ -43,6 +47,7 @@ export const dummyItems = [
 		description: 'Sweet and juicy.',
 		image:
 			'https://cdn.pixabay.com/photo/2015/05/26/17/42/pop-corn-785074_1280.jpg',
+		rating: 5,
 	},
 	{
 		id: '6',
@@ -51,6 +56,7 @@ export const dummyItems = [
 		price: 12,
 		description: 'Straiht from the cow farm',
 		image: 'https://cdn.pixabay.com/photo/2016/05/07/16/09/milk-1377564_1280.jpg',
+		rating: 2,
 	},
 	{
 		id: '7',
@@ -60,6 +66,7 @@ export const dummyItems = [
 		description: 'Our holes are the biggest',
 		image:
 			'https://cdn.pixabay.com/photo/2016/03/05/19/24/cheese-1238395_1280.jpg',
+		rating: 1,
 	},
 	{
 		id: '8',
@@ -69,6 +76,7 @@ export const dummyItems = [
 		description: 'Gourmets love it',
 		image:
 			'https://cdn.pixabay.com/photo/2020/07/27/17/49/cheese-5443041_1280.jpg',
+		rating: 4,
 	},
 	{
 		id: '9',
@@ -78,6 +86,7 @@ export const dummyItems = [
 		description: 'Nice, yellow, monkey style',
 		image:
 			'https://cdn.pixabay.com/photo/2018/09/24/20/12/bananas-3700718_1280.jpg',
+		rating: 5,
 	},
 	{
 		id: '10',
@@ -87,6 +96,7 @@ export const dummyItems = [
 		description: 'Crunchy, sweet and juicy',
 		image:
 			'https://cdn.pixabay.com/photo/2016/10/27/22/52/apples-1776744_1280.jpg',
+		rating: 2,
 	},
 	{
 		id: '11',
@@ -96,6 +106,7 @@ export const dummyItems = [
 		description: 'Best for orange fresh',
 		image:
 			'https://cdn.pixabay.com/photo/2014/02/23/16/41/oranges-273024_1280.jpg',
+		rating: 3,
 	},
 	{
 		id: '12',
@@ -105,5 +116,6 @@ export const dummyItems = [
 		description: 'Best for cocktails',
 		image:
 			'https://cdn.pixabay.com/photo/2018/11/11/15/42/pineapple-3808963_1280.jpg',
+		rating: 1,
 	},
 ];
--- app/products/ProductCard/ProductCard.tsx
+++ app/products/ProductCard/ProductCard.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Product as ProductType } from '../../interfaces/interfaces';
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { formatter } from '../../assets/helpers/formatter';
 import { capitalize } from '@/app/assets/helpers/capitalize';
+import Star from '../../assets/svg/Star';
 
 const ProductCard = ({
 	product,
@@ -12,10 +13,40 @@ const ProductCard = ({
 	product: ProductType;
 	addToCart: (item: ProductType) => void;
 }) => {
+	const [rating, setRating] = useState<number>(product.rating);
+	// const [starColor, setStarColor] = useState('transparent');
+
+	const handleRating = (selectedRating: number) => {
+		setRating(selectedRating);
+		sendRatingToServer(selectedRating);
+	};
+	const sendRatingToServer = (newRating: number) => {
+		// Make an API request to update the product rating in the database
+
+		// axios.post('/api/updateProductRating', { productId: product.id, rating: newRating })
+		//   .then((response) => {
+		//     // Handle success
+		//   })
+		//   .catch((error) => {
+		//     // Handle error
+		//   });
+
+		// For demonstration purposes, we'll just console log the new rating
+		console.log(`Rating updated to ${newRating}`);
+	};
+
 	return (
-		<li className='relative overflow-hidden'>
+		<li className='relative'>
 			<div className='badge-info opacity-75 absolute top-2 right-2 z-10 rounded-xl p-1'>
-				<p>Rating {'4/5'}</p>
+				<div className='flex'>
+					{[1, 2, 3, 4, 5].map(star => (
+						<Star
+							key={star}
+							fillColor={star <= rating ? 'yellow' : 'transparent'}
+							onClick={() => handleRating(star)}
+						/>
+					))}
+				</div>
 			</div>
 			<div className='card card-compact bg-base-100 shadow-xl'>
 				<figure>
